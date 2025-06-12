1a97b7d51af5feebc02b7e4a07b22657c04d7356
aysegularslan <arslanaysegull@outlook.com>
2025-06-12 23:51:05 +0300

    vite.config.js update

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/goit-js-hw-12/', // repo adı
     root: 'src',
     build: {
       sourcemap: true,
